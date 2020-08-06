cf66fb9f751cf728aae5899088b0bf9e7536ef34
phatdroid94@gmail.com <phatdroid94@gmail.com>
2020-08-06 19:27:34 +0700

    thread

--- app/src/main/java/com/example/thread02062020/MainActivity.java
+++ app/src/main/java/com/example/thread02062020/MainActivity.java
@@ -7,38 +7,79 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-
     int a , b  ,c;
+    MyFlag mMyFlag;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         a = b = c = 0;
 
+        mMyFlag = new MyFlag(0);
+
         Thread threadA = new Thread(new Runnable() {
             @Override
             public void run() {
-                for (int i = 1 ; i <= 10 ; i++){
-                    a = i;
-                    Log.d("BBB" , "A : " + i);
+                synchronized (mMyFlag){
+                    for (int i = 1 ; i <= 1000 ; ){
+                        if (mMyFlag.count == 0){
+                            a = i;
+                            Log.d("BBB" , "A : " + i);
+                            mMyFlag.count = 1;
+                            mMyFlag.notifyAll();
+                            i++;
+                        }else{
+                            try {
+                                mMyFlag.wait();
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
                 }
             }
         });
         Thread threadB = new Thread(new Runnable() {
             @Override
             public void run() {
-                for (int i = 1 ; i <= 10 ; i++){
-                    b = i;
-                    Log.d("BBB" , "B : " + i);
+                synchronized (mMyFlag){
+                    for (int i = 1 ; i <= 1000 ;){
+                       if (mMyFlag.count == 1){
+                           b = i;
+                           Log.d("BBB" , "B : " + i);
+                           mMyFlag.count = 2;
+                           mMyFlag.notifyAll();
+                           i++;
+                       }else{
+                           try {
+                               mMyFlag.wait();
+                           } catch (InterruptedException e) {
+                               e.printStackTrace();
+                           }
+                       }
+                    }
                 }
             }
         });
         Thread threadC = new Thread(new Runnable() {
             @Override
             public void run() {
-                for (int i = 1 ; i <= 10 ; i++){
-                    c = a + b;
-                    Log.d("BBB" , "c : " + i);
+                synchronized (mMyFlag){
+                    for (int i = 1 ; i <= 1000 ;){
+                        if (mMyFlag.count == 2){
+                            c = a + b;
+                            Log.d("BBB" , "c : " + c);
+                            mMyFlag.count = 0;
+                            mMyFlag.notifyAll();
+                            i++;
+                        }else{
+                            try {
+                                mMyFlag.wait();
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
                 }
             }
         });
---
+++ app/src/main/java/com/example/thread02062020/MyFlag.java
@@ -0,0 +1,9 @@
+package com.example.thread02062020;
+
+public class MyFlag {
+    int count;
+
+    public MyFlag(int count) {
+        this.count = count;
+    }
+}
